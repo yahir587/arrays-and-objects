b8e21986119b4446ebc3653af4523613261c4e0f
yahir587 <83249710+yahir587@users.noreply.github.com>
2022-11-11 15:56:48 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+// task 1: Create your own JavaScript object representing your
+//    favorite movie. Include at least 5 properties: movie title,
+//    director, release year, actors (make this an array with at
+//    least 3 elements), and length of movie in minutes.
+//    Feel free to add more!
+
+// task 2: After you have created your movie object, console log the
+//    title of your movie using dot notation.
+
+// task 3: Console log the director's name
+
+// task 4: Console log the release year.
+
+// task 5: Use bracket notation in combination with dot notation to
+//    access one of the actors and console log their name.
+
+// task 6: Add an actor to the arrays of actors. Console log the array
+//    to check the actor was added.
+
+// task 7: Oh, no! One of the actors has been cancelled! Remove one
+//    of the actors from the array. Console log the array to check
+//    the actor was removed.
+
+// task 8: An extended director's cut has been release for your movie!
+//    Add 20 minutes to the duration. Console log duration.
+
+let favMovie = {
+  movieName: "Poki Man",
+  director: "Trevor",
+  releaseYear: "2904",
+  actors: ["Yosef", "Eliot", "Sal"],
+  company: ["Nintendo", "FromSoftware", "BananaRepublic"],
+  duration: "9 hours"
+};
+console.log(favMovie.duration);
